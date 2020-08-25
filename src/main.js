fdfd3d751c8bce603a4f4e8d30dbe7e943092e81
Егор Тумаков <el.tumakov@gmail.com>
2020-08-25 10:02:55 +0300

    Добавляет модель для фильтра

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import SiteMenuView from "./view/site-menu.js";
 import FilterView from "./view/filter.js";
 import BoardPresenter from "./presenter/board.js";
 import TasksModel from "./model/tasks.js";
+import FilterModel from "./model/filter.js";
 import {generateTask} from "./mock/task.js";
 import {generateFilter} from "./mock/filter.js";
 import {render, RenderPosition} from "./utils/render.js";
@@ -13,6 +14,8 @@ const filters = generateFilter(tasks);
 
 const tasksModel = new TasksModel();
 tasksModel.setTasks(tasks);
+
+const filterModel = new FilterModel();
 
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
