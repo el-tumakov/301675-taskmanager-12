4ac1ac721cfd089a1b8b36acd43926b66a0238d8
Егор Тумаков <el.tumakov@gmail.com>
2020-08-25 10:07:23 +0300

    Рефактори фильтрацию
    - удаляет моки для фильтров
    - функции фильтрации переносит в утилитарный модуль
    - обучает учитывать выбранный фильтр в шаблоне фильтров
    - в представлении фильтро реализует механизм подписки на выбор нового фильтра

--- src/main.js
+++ src/main.js
@@ -4,13 +4,19 @@ import BoardPresenter from "./presenter/board.js";
 import TasksModel from "./model/tasks.js";
 import FilterModel from "./model/filter.js";
 import {generateTask} from "./mock/task.js";
-import {generateFilter} from "./mock/filter.js";
 import {render, RenderPosition} from "./utils/render.js";
 
 const TASK_COUNT = 25;
 
 const tasks = new Array(TASK_COUNT).fill().map(generateTask);
-const filters = generateFilter(tasks);
+
+const filters = [
+  {
+    type: `all`,
+    name: `ALL`,
+    count: 0
+  }
+];
 
 const tasksModel = new TasksModel();
 tasksModel.setTasks(tasks);
@@ -23,6 +29,6 @@ const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 const boardPresenter = new BoardPresenter(siteMainElement, tasksModel);
 
 render(siteHeaderElement, new SiteMenuView(), RenderPosition.BEFOREEND);
-render(siteMainElement, new FilterView(filters), RenderPosition.BEFOREEND);
+render(siteMainElement, new FilterView(filters, `all`), RenderPosition.BEFOREEND);
 
 boardPresenter.init();
