0150443f150b114d80a9591512a898af433c1753
Егор Тумаков <el.tumakov@gmail.com>
2020-08-29 10:45:45 +0300

    Рефакторит инициализацию приложения. WIP
    - удаляет моки
    - группирует объявления переменных и вызовы
    - записывает данные в модель после получения с сервера
    
    WIP: данные приходят, но не отображаются, потому что модель не
    уведомляет наблюдателей, что данные пришли

--- src/main.js
+++ src/main.js
@@ -3,33 +3,26 @@ import BoardPresenter from "./presenter/board.js";
 import FilterPresenter from "./presenter/filter.js";
 import TasksModel from "./model/tasks.js";
 import FilterModel from "./model/filter.js";
-import {generateTask} from "./mock/task.js";
 import {render, RenderPosition} from "./utils/render.js";
 import Api from "./api.js";
 
-const TASK_COUNT = 25;
 const AUTHORIZATION = `Basic qC2gh6df5qrl1sa5p`;
 const END_POINT = `https://12.ecmascript.pages.academy/task-manager`;
-
-const tasks = new Array(TASK_COUNT).fill().map(generateTask);
-const api = new Api(END_POINT, AUTHORIZATION);
-
-api.getTasks().then((tasks) => {
-  console.log(tasks);
-});
-
-const tasksModel = new TasksModel();
-tasksModel.setTasks(tasks);
-
-const filterModel = new FilterModel();
 
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
-render(siteHeaderElement, new SiteMenuView(), RenderPosition.BEFOREEND);
+const api = new Api(END_POINT, AUTHORIZATION);
+
+const tasksModel = new TasksModel();
+const filterModel = new FilterModel();
+
+const siteMenuComponent = new SiteMenuView();
 
 const boardPresenter = new BoardPresenter(siteMainElement, tasksModel, filterModel);
 const filterPresenter = new FilterPresenter(siteMainElement, filterModel, tasksModel);
+
+render(siteHeaderElement, siteMenuComponent, RenderPosition.BEFOREEND);
 
 filterPresenter.init();
 boardPresenter.init();
@@ -37,4 +30,8 @@ boardPresenter.init();
 document.querySelector(`#control__new-task`).addEventListener(`click`, (evt) => {
   evt.preventDefault();
   boardPresenter.createTask();
+});
+
+api.getTasks().then((tasks) => {
+  tasksModel.setTasks(tasks);
 });
